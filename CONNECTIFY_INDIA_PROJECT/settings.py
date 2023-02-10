a0a1134d92c0f26f199308eaa5da58e5d030df1b
connectifyindia <support@connectifyindia.com>
2023-02-10 10:38:36 +0530

    use sqlite3

--- CONNECTIFY_INDIA_PROJECT/settings.py
+++ CONNECTIFY_INDIA_PROJECT/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'CONNECTIFY_INDIA_PROJECT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'connectify_india_db',
-        'USER':'root',
-        'PASSWORD':'root',
-
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'connectify_india_db',
+#         'USER':'root',
+#         'PASSWORD':'root',
+
+
+#     }
+# }
 
 
 
